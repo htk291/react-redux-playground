f3137e882fdf59195a81480ca5c0a9d51ba2596f
htk291 <htk291@gmail.com>
2017-01-11 09:23:09 +0900

    search with condition

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,18 +3,32 @@ import axios from 'axios'
 import { connect } from 'react-redux'
 import { update } from 'REDUX/actions'
 
-class List extends React.Component {
+class Conditions extends React.Component {
   search = e => {
-    axios.get('data.json')
+    this.props.search({value: this.refs.input.value})
+  }
+  render() {
+    return (
+      <div>
+        <input type='text' ref='input' defaultValue='' />
+        <button onClick={this.search}>search</button>
+      </div>
+    )
+  }
+}
+
+class List extends React.Component {
+  search = args => {
+    axios.get('data.json', {params: args})
       .then(res => {this.props.update(res.data)})
   }
   render() {
     return (
       <div>
-        <button onClick={this.search}>search</button>
+        <Conditions search={this.search} />
         {this.props.list.map(x => (
           <div key={x.key}>
-            <span >{x.a}</span>
+            <span>{x.a}</span>
             <span>{x.b}</span>
             <span>{x.c}</span>
           </div>
